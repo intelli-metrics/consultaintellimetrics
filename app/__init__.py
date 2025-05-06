2871c4bb3bef12a34b20d03590c8f517e92a8e8d
Augusto Peres <32046127+augustoapg@users.noreply.github.com>
2025-05-05 21:36:58 -0400

    conserta erro com cors (#10)

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    
+    # Configure CORS
+    CORS(app, resources={r"*": {"origins": "*"}})
     
     # Register blueprints or routes
     from .routes import main
