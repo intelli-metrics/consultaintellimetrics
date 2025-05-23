9fbf78c6b298754239dfaf7aa3688625ff3dc9b0
Augusto Peres <augustoapg@gmail.com>
2025-05-22 20:32:37 -0400

    update views security invokers

---
+++ supabase/migrations/20250523003002_fix_view_security_invoker.sql
@@ -0,0 +1,4 @@
+ALTER VIEW "VwRelDadosDispositivo" set (security_invoker = true);
+ALTER VIEW "VwRelHistoricoDispositivoProduto" set (security_invoker = true);
+ALTER VIEW "VwTbDestinatarioDispositivo" set (security_invoker = true);
+ALTER VIEW "VwTbPosicaoAtual" set (security_invoker = true);
