f9c290e92dfd7b1ef9022da237b95ffc26f884d6
Augusto Peres <augustoapg@gmail.com>
2025-07-07 19:54:16 -0400

    adiciona rls para service role

---
+++ supabase/migrations/20250707003959_add_service_rls.sql
@@ -0,0 +1,72 @@
+ALTER POLICY "Somente usuarios com acesso ao cliente" ON "public"."TbDestinatario" TO "authenticated" USING (
+  (
+    (
+      (
+        SELECT profiles.role
+        FROM profiles
+        WHERE (profiles.id = auth.uid())
+      ) = 'service'::role
+    )
+    OR (
+      "cdCliente" = ANY (
+        ARRAY(
+          SELECT get_clientes_user(auth.uid()) AS get_clientes_user
+        )
+      )
+    )
+  )
+);
+ALTER POLICY "Somente usuarios com acesso ao cliente" ON "public"."TbDispositivo" TO "authenticated" USING (
+  (
+    (
+      (
+        SELECT profiles.role
+        FROM profiles
+        WHERE (profiles.id = auth.uid())
+      ) = 'service'::role
+    )
+    OR (
+      "cdCliente" = ANY (
+        ARRAY(
+          SELECT get_clientes_user(auth.uid()) AS get_clientes_user
+        )
+      )
+    )
+  )
+);
+ALTER POLICY "Somente usuarios com acesso ao cliente" ON "public"."TbPosicao" TO "authenticated" USING (
+  (
+    (
+      (
+        SELECT profiles.role
+        FROM profiles
+        WHERE (profiles.id = auth.uid())
+      ) = 'service'::role
+    )
+    OR (
+      "cdDispositivo" = ANY (
+        ARRAY(
+          SELECT get_clientes_user_by_dispositivo(auth.uid()) AS get_clientes_user_by_dispositivo
+        )
+      )
+    )
+  )
+);
+ALTER POLICY "Somente usuarios com acesso ao cliente" ON "public"."TbProduto" TO "authenticated" USING (
+  (
+    (
+      (
+        SELECT profiles.role
+        FROM profiles
+        WHERE (profiles.id = auth.uid())
+      ) = 'service'::role
+    )
+    OR (
+      "cdDispositivo" = ANY (
+        ARRAY(
+          SELECT get_clientes_user_by_dispositivo(auth.uid()) AS get_clientes_user_by_dispositivo
+        )
+      )
+    )
+  )
+);
